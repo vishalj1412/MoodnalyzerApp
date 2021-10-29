26f10a761593283d66ec882fa84eadae6c9ee3fe
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-29 18:39:38 +0530

    Added: mood analyzer application

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -3,16 +3,23 @@ package org.exceptions;
 public class MoodAnalyzer{
 
     String mood;
-    //analyzemood method for getting meassge
+    /*
+    analyzemood method for getting meassge
+    */
     public String analyzeMood(String message) {
          this.mood = message;
-        //analyze mood by checking message
-
+        /*
+        analyze mood by checking message
+*/
         if (mood.contains("sad")) {
-            //if message has keyword sad then mood is sad
+            /*
+            if message has keyword sad then mood is sad
+            */
             return "sad";
         } else
-            //for any mood it returns happy
+            /*
+            for any mood it returns happy
+            */
             return "Happy" ;
     }
 }
